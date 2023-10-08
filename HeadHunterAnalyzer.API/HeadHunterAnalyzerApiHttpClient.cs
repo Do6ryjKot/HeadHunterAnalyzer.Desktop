96c9990322fe068767d4c041b82745d00b811a8b
Do6ryjKot <arkhanghel@mail.ru>
2023-10-08 13:01:04 +0300

    T5 Done. Errors are shown to the user correctly.

--- HeadHunterAnalyzer.API/HeadHunterAnalyzerApiHttpClient.cs
+++ HeadHunterAnalyzer.API/HeadHunterAnalyzerApiHttpClient.cs
@@ -1,4 +1,5 @@
-﻿using Entities.DataTransferObjects;
+﻿using Contracts.Logger;
+using Entities.DataTransferObjects;
 using Newtonsoft.Json;
 using System.Net.Http.Json;
 
@@ -7,9 +8,11 @@ namespace HeadHunterAnalyzer.API {
 	public class HeadHunterAnalyzerApiHttpClient {
 
 		private readonly HttpClient _client;
+		private readonly ILoggerManager _logger;
 
-		public HeadHunterAnalyzerApiHttpClient(HttpClient client) {
+		public HeadHunterAnalyzerApiHttpClient(HttpClient client, ILoggerManager logger) {
 			_client = client;
+			_logger = logger;
 		}
 
 		public async Task<T?> PostAsync<T>(string uri, JsonContent content) {
@@ -28,10 +31,26 @@ namespace HeadHunterAnalyzer.API {
 
 		private async Task<T?> DeserializeResponse<T>(HttpResponseMessage response) {
 
-			if (!response.IsSuccessStatusCode)
-				throw new Exception("Произошла ошибка при обращении к API HeadHunterAnalyzer");
-
 			string json = await response.Content.ReadAsStringAsync();
+
+			if (!response.IsSuccessStatusCode) {
+
+				var error = JsonConvert.DeserializeObject<ResultDetails>(json);
+				var errorMessage = $"Произошла ошибка при обращении к API HeadHunterAnalyzer, " +
+					$"метод: {response.RequestMessage?.RequestUri}";
+
+				if (error == null) {
+
+					_logger.LogError(errorMessage);
+
+					throw new Exception(errorMessage);
+				}
+
+				errorMessage += $", ошибка: {error.Message}";
+				_logger.LogError(errorMessage);
+
+				throw new Exception(error.Message);
+			}
 
 			return JsonConvert.DeserializeObject<T>(json);
 		}
